ed516393f8f9fb3d78712b2797326a9651c19f4c
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-08-18 20:47:52 +0300

    progress

--- lib/Screens/main_Menu.dart
+++ lib/Screens/main_Menu.dart
@@ -7,46 +7,51 @@ import 'package:gwent/Screens/game_screen.dart';
 import 'package:gwent/Screens/rules_screen.dart';
 import 'package:gwent/Screens/setup_deck_menu.dart';
 
-
 class MainMenu extends StatelessWidget {
   static const routeName = '/main-menu';
 
   @override
   Widget build(BuildContext context) {
     portraitMode();
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.black,
-          child: Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Image(
-                  fit: BoxFit.fill,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.black,
+            child: Stack(
+              children: <Widget>[
+                // Positioned.fill(
+                //   child: Image(
+                //     fit: BoxFit.fill,
+                //     image: AssetImage('GameAssets/Back/Back.png'),
+                //   ),
+                // ),
+                Image(
                   image: AssetImage('GameAssets/Back/Back.png'),
+                  fit: BoxFit.contain,
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  MenuButtons(
-                    buttonTitle: 'PLAY GAME',
-                    routeToNavigateAt: Game.routeName,
-                  ),
-                  MenuButtons(
-                    buttonTitle: 'HOW TO PLAY',
-                    routeToNavigateAt: Rules.routeName,
-                  ),
-                  MenuButtons(
-                    buttonTitle: 'SETUP DECKS',
-                    routeToNavigateAt: SetupDecksMenu.routeName,
-                  ),
-                ],
-              ),
-            ],
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MenuButtons(
+                      buttonTitle: 'PLAY GAME',
+                      routeToNavigateAt: Game.routeName,
+                    ),
+                    MenuButtons(
+                      buttonTitle: 'HOW TO PLAY',
+                      routeToNavigateAt: Rules.routeName,
+                    ),
+                    MenuButtons(
+                      buttonTitle: 'SETUP DECKS',
+                      routeToNavigateAt: SetupDecksMenu.routeName,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/widgets/menu_buttons.dart
+++ lib/widgets/menu_buttons.dart
@@ -10,33 +10,45 @@ class MenuButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
-      child: Card(
-        elevation: 5,
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, routeToNavigateAt);
-          },
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.amber[50],
-              borderRadius: BorderRadius.circular(100.0),
-            ),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text(
-                buttonTitle,
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.black54
-                  ),
-              ),
-            ]
-            ),
-          ),
+    return Container(
+      width: MediaQuery.of(context).size.width * .6,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.black54,
+          width: 2,
         ),
       ),
     );
+
+    // return Padding(
+    //   padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
+    //   child: Card(
+    //     elevation: 5,
+    //     child: GestureDetector(
+    //       onTap: () {
+    //         Navigator.pushNamed(context, routeToNavigateAt);
+    //       },
+    //       child: Container(
+    //         padding: EdgeInsets.all(10),
+    //         decoration: BoxDecoration(
+    //           color: Colors.amber[50],
+    //           borderRadius: BorderRadius.circular(100.0),
+    //         ),
+    //         child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+    //           Text(
+    //             buttonTitle,
+    //             style: TextStyle(
+    //               fontSize: 40,
+    //               color: Colors.black54
+    //               ),
+    //           ),
+    //         ]
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
