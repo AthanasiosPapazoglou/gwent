2964c71d814d762aca66778976a01bb1df726838
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-09-22 17:08:41 +0300

    Beautification (Deck Selection Page)

--- lib/Screens/setup_deck_menu.dart
+++ lib/Screens/setup_deck_menu.dart
@@ -9,10 +9,22 @@ import 'package:gwent/App-Utilities/constants.dart';
 import 'package:gwent/App-Utilities/deckDataBase.dart';
 import 'package:gwent/App-Utilities/enums.dart';
 
-class SetupDecksMenu extends StatelessWidget {
+class SetupDecksMenu extends StatefulWidget {
   static const routeName = '/Setup-Decks-screen';
 
+  @override
+  State<SetupDecksMenu> createState() => _SetupDecksMenuState();
+}
+
+class _SetupDecksMenuState extends State<SetupDecksMenu> {
   Offset triggerPoint;
+  int selection;
+
+  void changeHighLightedDeck(int selectedIndex) {
+    setState(() {
+      selection = selectedIndex;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,45 +44,67 @@ class SetupDecksMenu extends StatelessWidget {
         triggerPoint = null;
       },
       child: Scaffold(
-        body: Center(
-          child: Container(
-            color: Colors.black54,
-            child: Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <DeckButton>[
-                  DeckButton(
-                    imageURL: kMonBackAD,
-                    deckName: 'Monsters',
-                    deckColors: Colors.red.shade400,
-                    subtitleText: kMonDesc,
-                    assets: deckAssets.monsters
-                  ),
-                  DeckButton(
-                    imageURL: kNilfBackAD,
-                    deckName: 'Nilfgaardian Empire',
-                    deckColors: Colors.yellow.shade400,
-                    subtitleText: kNilfDesc,
-                    assets: deckAssets.nilfgaard,
-                  ),
-                  DeckButton(
-                    imageURL: kNorthBackAD,
-                    deckName: 'Northern Realms',
-                    deckColors: Colors.blue.shade400,
-                    subtitleText: kNorthDesc,
-                    assets: deckAssets.northernRealms,
-                  ),
-                  DeckButton(
-                    imageURL: kScoiaBackAD,
-                    deckName: 'Scoia\'tael',
-                    deckColors: Colors.green.shade400,
-                    subtitleText: kScoiaDesc,
-                    assets: deckAssets.scoiatael,
-                  ),
-                ],
+        backgroundColor: Colors.blueGrey,
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 25),
+                DeckButton(
+                  imageURL: kMonBackAD,
+                  deckName: 'Monsters',
+                  deckColors: Colors.red.shade400,
+                  subtitleText: kMonDesc,
+                  assets: deckAssets.monsters,
+                  highLightChanger: changeHighLightedDeck,
+                  selectedIndex: selection,
+                  deckHighlightIndex: 0,
+                ),
+                SizedBox(height: 50),
+                DeckButton(
+                  imageURL: kNilfBackAD,
+                  deckName: 'Nilfgaardian Empire',
+                  deckColors: Colors.yellow.shade400,
+                  subtitleText: kNilfDesc,
+                  assets: deckAssets.nilfgaard,
+                  highLightChanger: changeHighLightedDeck,
+                  selectedIndex: selection,
+                  deckHighlightIndex: 1,
+                ),
+                SizedBox(height: 50),
+                DeckButton(
+                  imageURL: kNorthBackAD,
+                  deckName: 'Northern Realms',
+                  deckColors: Colors.blue.shade400,
+                  subtitleText: kNorthDesc,
+                  assets: deckAssets.northernRealms,
+                  highLightChanger: changeHighLightedDeck,
+                  selectedIndex: selection,
+                  deckHighlightIndex: 2,
+                ),
+                SizedBox(height: 50),
+                DeckButton(
+                  imageURL: kScoiaBackAD,
+                  deckName: 'Scoia\'tael',
+                  deckColors: Colors.green.shade400,
+                  subtitleText: kScoiaDesc,
+                  assets: deckAssets.scoiatael,
+                  highLightChanger: changeHighLightedDeck,
+                  selectedIndex: selection,
+                  deckHighlightIndex: 3,
+                ),
+              ],
+            ),
+            Positioned(
+              top: 16,
+              left: 16,
+              child: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/widgets/deck_select_button.dart
+++ lib/widgets/deck_select_button.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers, use_key_in_widget_constructors, file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gwent/Providers/customDecks.dart';
 import 'package:gwent/Screens/customise_deck.dart';
 import 'package:gwent/widgets/customiseDeck/deck_grid_view.dart';
@@ -8,51 +9,84 @@ import 'package:gwent/App-Utilities/deckDataBase.dart';
 import 'package:gwent/App-Utilities/enums.dart';
 import 'package:provider/provider.dart';
 
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 class DeckButton extends StatelessWidget {
   final String imageURL;
   final String deckName;
   final Color deckColors;
   final String subtitleText;
   final deckAssets assets;
+  final Function highLightChanger;
+  final int selectedIndex;
+  final int deckHighlightIndex;
 
   DeckButton({
-      @required this.imageURL,
-      @required this.deckName,
-      @required this.deckColors,
-      @required this.subtitleText,
-      @required this.assets
-      });
+    @required this.imageURL,
+    @required this.deckName,
+    @required this.deckColors,
+    @required this.subtitleText,
+    @required this.assets,
+    @required this.highLightChanger,
+    @required this.selectedIndex,
+    @required this.deckHighlightIndex
+  });
 
   @override
   Widget build(BuildContext context) {
     final customDecks = Provider.of<CustomDecks>(context);
     return InkWell(
       onTap: () {
-        customDecks.playerDeckSelection = assets;
-        Navigator.pushNamed(context, CustomiseDeck.routeName);
+        highLightChanger(deckHighlightIndex);
+        // customDecks.playerDeckSelection = assets;
+        // Navigator.pushNamed(context, CustomiseDeck.routeName);
       },
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 16),
-        child: Card(
-          elevation: 4,
-          color: deckColors,
-          child: ListTile(
-            trailing: Image(
-              height: 40,
-              width: 22.5,
-              image: AssetImage(imageURL),
-            ),
-            title: Text(
-              deckName,
-              style: TextStyle(fontSize: 30, color: Colors.black87),
-            ),
-            subtitle: Text(
-              subtitleText,
-              style: TextStyle(fontSize: 16),
-            ),
+        padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+        child: Container(
+          width: double.infinity,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 3),
+            boxShadow: [
+              if(selectedIndex == deckHighlightIndex)
+              BoxShadow(
+                color: deckColors.withOpacity(.8),
+                spreadRadius: 6,
+                blurRadius: 6,
+                offset: Offset(0, 0),
+              ),
+            ],
+          ),
+          child: Image(
+            image: AssetImage(imageURL),
+            fit: BoxFit.cover,
           ),
         ),
       ),
+      // child: Padding(
+      //   padding: EdgeInsets.symmetric(horizontal: 15, vertical: 16),
+      //   child: Card(
+      //     elevation: 4,
+      //     color: deckColors,
+      //     child: ListTile(
+      //       trailing: Image(
+      //         height: 40,
+      //         width: 22.5,
+      //         image: AssetImage(imageURL),
+      //       ),
+      //       title: Text(
+      //         deckName,
+      //         style: TextStyle(fontSize: 30, color: Colors.black87),
+      //       ),
+      //       subtitle: Text(
+      //         subtitleText,
+      //         style: TextStyle(fontSize: 16),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
