99b0e8fa6ddde5667182143cde381ec69f336d30
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2023-01-27 14:07:14 +0200

    fixed alignments for card placements on pixel 3

--- lib/Screens/game_screen.dart
+++ lib/Screens/game_screen.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:gwent/App-Utilities/enums.dart';
 import 'package:gwent/App-Utilities/functions.dart';
 import 'package:gwent/widgets/GamePlay/bRow_list_view.dart';
 import 'package:gwent/widgets/GamePlay/hand_list_view.dart';
 import 'package:gwent/Providers/customDecks.dart';
 import 'package:provider/provider.dart';
 import 'package:gwent/Providers/customDecks.dart';
+
+import '../App-Utilities/constants.dart';
 
 class Game extends StatefulWidget {
   static const routeName = '/game-screen';
@@ -18,7 +21,6 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
-    
     @override
     void initState() {
       super.initState();
@@ -33,6 +35,30 @@ class _GameState extends State<Game> {
 
     final customDecks = Provider.of<CustomDecks>(context);
     final size = MediaQuery.of(context).size;
+    String playerLeaderName;
+    String playerLeaderBaseDirectory;
+
+    switch (customDecks.playerDeckSelection) {
+      case deckAssets.monsters:
+        playerLeaderName = customDecks.selectedMonstersLeader;
+        playerLeaderBaseDirectory = kMonLeadersAD;
+        break;
+
+      case deckAssets.nilfgaard:
+        playerLeaderName = customDecks.selectedNilfggardLeader;
+        playerLeaderBaseDirectory = kNilfLeadersAD;
+        break;
+
+      case deckAssets.northernRealms:
+        playerLeaderName = customDecks.selectedNorthernRealmsLeader;
+        playerLeaderBaseDirectory = kNorthLeadersAD;
+        break;
+
+      case deckAssets.scoiatael:
+        playerLeaderName = customDecks.selectedScoiataelLeader;
+        playerLeaderBaseDirectory = kScoiaLeadersAD;
+        break;
+    }
 
     if (customDecks.gameHasBegun) {
       return SafeArea(
@@ -45,7 +71,67 @@ class _GameState extends State<Game> {
                 height: MediaQuery.of(context).size.height,
                 fit: BoxFit.cover,
               ),
-              
+              Positioned(
+                bottom: 0,
+                left: 220,
+                child: Align(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 70,
+                    child: ListView.builder(
+                      itemCount: customDecks.handCards.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        String _assetDirectory;
+
+                        if (customDecks.handCards[index].id <= 20) {
+                          _assetDirectory = kSpecialCardsAD;
+                        } else if (customDecks.handCards[index].id <= 30) {
+                          _assetDirectory = kNeutralUnitsAD;
+                        } else {
+                          switch (customDecks.playerDeckSelection) {
+                            case deckAssets.monsters:
+                              _assetDirectory = kMonUnitsAD;
+                              break;
+
+                            case deckAssets.nilfgaard:
+                              _assetDirectory = kNilfUnitsAD;
+                              break;
+
+                            case deckAssets.northernRealms:
+                              _assetDirectory = kNorthUnitsAD;
+                              break;
+
+                            case deckAssets.scoiatael:
+                              _assetDirectory = kScoiaUnitsAD;
+                              break;
+                          }
+                        }
+
+                        return Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(4, 0, 4, 0),
+                          child: SizedBox(
+                            width: 36,
+                            height: 64,
+                            child: Image.asset(
+                                '$_assetDirectory${customDecks.handCards[index].cardName}'),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 18,
+                left: 50,
+                child: SizedBox(
+                  child: Image.asset(
+                      '$playerLeaderBaseDirectory$playerLeaderName'),
+                  width: 45,
+                  height: 80,
+                ),
+              ),
             ],
           ),
         ),
